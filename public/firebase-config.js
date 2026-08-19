bfffd56d16035a11b123f06e39ce05111378546b
gabrielduclos <gabriel.duclos@icloud.com>
2026-08-19 16:36:13 -0300

    1.0.0 CONFIGURADO FIREBASE

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -2,8 +2,8 @@
 // Firebase Console > Configurações do projeto > Seus apps > Web.
 // Estes valores identificam o app cliente; NÃO coloque service account aqui.
 window.UORQUI_FIREBASE_CONFIG = {
-  apiKey: "COLE_AQUI",
+  apiKey: "AIzaSyDveHG5tmlhFymGoG04hzCX5nlKBNawzkg",
   authDomain: "uorqui-ba281.firebaseapp.com",
   projectId: "uorqui-ba281",
-  appId: "COLE_AQUI"
+  appId: "1:819254957425:web:05ab6ea863bbc1038dd2d0"
 };
