dda36c5d6dd9dd20611b0e20d19a505f16cda918
gabrielduclos <gabriel.duclos@icloud.com>
2026-08-21 10:13:05 -0300

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,7 +1,7 @@
 // Configuração pública do Firebase Web App.
 // Em seu repositório real, mantenha aqui os valores que você já configurou.
 window.UORQUI_FIREBASE_CONFIG = {
-  apiKey: "BGnBQagsExOlNVyzO9pF2hDuZmhHJZuH7dpSxsLek08W_6UJCfDtX_bF7LYGAoWJ7oP58KnqrRJVBrRO1BXYS1g",
+  apiKey: "AIzaSyDveHG5tmlhFymGoG04hzCX5nlKBNawzkg",
   authDomain: "uorqui-ba281.firebaseapp.com",
   projectId: "uorqui-ba281",
   appId: "1:819254957425:web:05ab6ea863bbc1038dd2d0"
