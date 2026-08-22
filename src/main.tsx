e0cf4cdf3c21a125cb39d6d40c2548941352bac0
gabrielduclos <gabriel.duclos@icloud.com>
2026-08-22 08:21:14 -0300

    style: load reply enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./comment-replies.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
