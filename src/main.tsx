badf53fbbcc3f91336ec8a39cfecd0d87bc931d1
gabrielduclos <gabriel.duclos@icloud.com>
2026-08-25 14:03:47 -0300

    feat: polish mention labels

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./product-v121";
 import "./product-v121-enhancements";
 import "./company-lookup";
 import "./product-v121-media";
+import "./mention-label-v121";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
