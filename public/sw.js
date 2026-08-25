3adec160fb19aa442efdd3ed8e5f220235391d70
gabrielduclos <gabriel.duclos@icloud.com>
2026-08-25 17:28:37 -0300

    release: refresh PWA cache for 1.2.23

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "uorqui-react-v1.2.22";
+const CACHE = "uorqui-react-v1.2.23";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(
   caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE && k.startsWith("uorqui-react-")).map((k) => caches.delete(k)))).then(() => self.clients.claim())
